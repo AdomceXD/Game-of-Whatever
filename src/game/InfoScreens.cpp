0a4bd0ed94a79fefeadf7e57949745a22923af6c
Adam <117942532+AdomceXD@users.noreply.github.com>
2025-08-29 00:27:26 +0300

    Add startscreen and screensplit

--- src/game/InfoScreens.cpp
+++ src/game/InfoScreens.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 #include <string>
+#include <sys/ioctl.h>
 #include "InfoScreens.h"
 
+void ScreenSplit(int a){
+    struct winsize w;
+    ioctl(0, TIOCGWINSZ, &w);
+    int leftMargin = w.ws_col / 2 - a;
+    for (int i=0;i<leftMargin;i++){
+        std::cout << ' ';
+    }
+    //How this works: Take the length of your text, devide by 2 and insert the function next to the text.
+}
+
 void Help(){
-std::cout << "List of commands:\n";
-std::cout << "Help - Display help screen\nClear - Clear screen\nExit - Exit game\n";
+printf("List of commands:\nHelp - Display help screen\nClear - Clear screen\nExit - Exit game\n");
 }
 
 void Stats(){
@@ -16,5 +26,12 @@ void Credits(){
 }
 
 void StartScreen(){
+ScreenSplit(16);
+printf("  ________       __      __     \n");ScreenSplit(16);
+printf(" /  _____/  ____/  \\    /  \\  \n");ScreenSplit(16);
+printf("/   \\  ___ /  _ \\   \\/\\/   /\n");ScreenSplit(16);
+printf("\\    \\_\\  (  <_> )        /  \n");ScreenSplit(16);
+printf(" \\______  /\\____/ \\__/\\  /  \n");ScreenSplit(16);
+printf("        \\/             \\/     \n");
 
 }
